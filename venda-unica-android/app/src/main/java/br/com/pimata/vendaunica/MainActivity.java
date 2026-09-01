ef0dd89aea479049f20554c21f819c1c443b103a
bradocki2020 <zelutinho19@gmail.com>
2026-09-01 13:42:17 -0300

    Fix JavascriptInterface import for Venda Unica v1.3.2

--- venda-unica-android/app/src/main/java/br/com/pimata/vendaunica/MainActivity.java
+++ venda-unica-android/app/src/main/java/br/com/pimata/vendaunica/MainActivity.java
@@ -1,6 +1,6 @@
 package br.com.pimata.vendaunica;
 
-import android.annotation.JavascriptInterface;
+import android.webkit.JavascriptInterface;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
